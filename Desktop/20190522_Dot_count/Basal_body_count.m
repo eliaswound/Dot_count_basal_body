eedb2d56474d426b2539f996f5e32a6636caf0fa
Elias Guan <elias.guan@mail.utortonto.ca>
2019-05-23 13:26:42 -0400

    Finished!

--- Desktop/20190522_Dot_count/Basal_body_count.m
+++ Desktop/20190522_Dot_count/Basal_body_count.m
@@ -17,7 +17,8 @@ axis equal;
 % Using standard deviation as noise, and use mean as signal 
 threshhold=input('please input the threshold you want to use(0-255)');
 level=threshhold/255;
-
+% alternative way to get level
+% level=graythresh(image)
 % 2.2 Create binary image 
 bw=im2bw(image,level);
 figure(2000);
@@ -28,28 +29,35 @@ axis equal;
 
 %3.0 Calculate region area
 s=regionprops(bw,'Area');
-bw2=imfill(bw,'holes');
-figure(2000);
-subplot(2,2,3);
-imshow(bw2);
-title('Red Binary Filled')
-axis equal;
-sfill=regionprops(bw,'Area');
-a=sum(sum(bw));
-b=sum(sum(bw2));
-bw3=bw2-bw;
-figure(2000)
-subplot(2,2,4);
-imshow(bw3);
-title('holes filled');
-axis equal;
+%alternative fill hole method
+%bw2=imfill(bw,'holes');
+%figure(2000);
+%subplot(2,2,3);
+%title('Red Binary Filled')
+%axis equal;
+%sfill=regionprops(bw,'Area');
+%a=sum(sum(bw));
+%b=sum(sum(bw2));
+%bw3=bw2-bw;
+%figure(2000)
+%subplot(2,2,4);
+%imshow(bw3);
+%title('holes filled');
+%axis equal;
 
-
-
-
-
-
-%value = input('please input the threshold you want to use(0-255)');
-%level=value/255;
-%bw=im2bw(image,level);
-
+%get length of the tall vector
+sz=sum(size(s))-1;
+%assign stuct values into vector 
+TotalArea=zeros(1,sz);
+for i=1:sz
+    TotalArea(1,i)=gather(s(i).Area);
+end
+threshhold2=input('please input the threshold you want to use(min)');
+threshhold3=input('pleas input the threshhold you want to us (max)');
+EffectAreaCount=0;
+for i=1:sz
+    if TotalArea(1,i)>threshhold2 && TotalArea(1,i)<threshhold3
+        EffectAreaCount=EffectAreaCount+1;
+    end
+end
+EffectAreaCount;
